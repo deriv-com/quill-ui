ce9bdb61a715a3dcbba3051d26c250d7db5c7f22
meenu-deriv <meenakshi@regentmarkets.com>
2024-03-06 10:58:46 +0400

    chore: implement theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BodyText, H1, Hero, Typography } from "../lib/components/Typography";
+import { H1, Hero, Typography } from "../lib/components/Typography";
+import { ThemeProvider } from "../lib/components/providers/theme/themeProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <Typography as="span">Quill UI</Typography>
-        <Hero>Quill UI</Hero>
-        <H1>Quill UI</H1>
-        <H1 as="div">Quill UI - h1 as div</H1>
-        <H1 as="span">Quill UI - h1 as span</H1>
+        <ThemeProvider>
+            <div
+                style={{
+                    background: `var(--semantic-color-background-secondary-base)`,
+                }}
+            >
+                <Typography as="span">Quill UI</Typography>
+                <Hero>Quill UI</Hero>
+                <H1>Quill UI</H1>
+                <H1 as="div">Quill UI - h1 as div</H1>
+                <H1 as="span">Quill UI - h1 as span</H1>
+            </div>
+        </ThemeProvider>
     </React.StrictMode>,
 );
