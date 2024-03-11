94820b3f9266c27cc04ebe1074fb763c80becc85
jia-deriv <jiawei@regentmarkets.com>
2024-03-11 15:39:08 +0800

    chore: change back to light theme

--- lib/components/providers/theme/themeContext.tsx
+++ lib/components/providers/theme/themeContext.tsx
@@ -3,7 +3,7 @@ import React, { type Dispatch, type SetStateAction } from "react";
 export type Theme = "dark" | "light";
 
 export const initialThemeState = {
-    theme: "dark" as Theme,
+    theme: "light" as Theme,
     setTheme: (() => null) as Dispatch<SetStateAction<Theme>>,
     toggleTheme: (() => null) as () => void,
 };
