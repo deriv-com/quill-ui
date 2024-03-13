21b00bfaf015c1282596f40600dceea8d6219a08
jia-deriv <jiawei@regentmarkets.com>
2024-03-13 17:17:22 +0800

    chore: cleanup code

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,12 @@ import Button from "./button";
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <ThemeProvider>
-            <div className="--semantic-color-background-primary-base">
-                <Button />
-                <Text as="span">Quill UI</Text>
-                <Heading.Hero>Quill UI</Heading.Hero>
-                <Heading.H1>Quill UI</Heading.H1>
-                <Heading.H1 as="div">Quill UI - h1 as div</Heading.H1>
-                <Heading.H1 as="span">Quill UI - h1 as span</Heading.H1>
-            </div>
+            <Button />
+            <Text as="span">Quill UI</Text>
+            <Heading.Hero>Quill UI</Heading.Hero>
+            <Heading.H1>Quill UI</Heading.H1>
+            <Heading.H1 as="div">Quill UI - h1 as div</Heading.H1>
+            <Heading.H1 as="span">Quill UI - h1 as span</Heading.H1>
         </ThemeProvider>
     </React.StrictMode>,
 );
