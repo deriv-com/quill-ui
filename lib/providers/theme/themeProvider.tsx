c93303378bae2f8f646ff781bc773b5fbec0e7a4
jia-deriv <jiawei@regentmarkets.com>
2024-03-13 17:15:49 +0800

    chore: add font import and theme background

--- lib/providers/theme/themeProvider.tsx
+++ lib/providers/theme/themeProvider.tsx
@@ -28,7 +28,9 @@ export const ThemeProvider = ({ children, theme }: ThemeProviderProps) => {
 
     return (
         <ThemeContext.Provider value={{ theme: currentTheme, toggleTheme }}>
-            <section className={`theme--${currentTheme}`}>{children}</section>
+            <section className={`theme--${currentTheme} theme-background`}>
+                {children}
+            </section>
         </ThemeContext.Provider>
     );
 };
