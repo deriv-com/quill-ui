6cbcee102b0bdfef081037c7632b0b88d02f1fff
jia-deriv <jiawei@regentmarkets.com>
2024-03-15 17:17:55 +0800

    feat: add background color classes

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,14 @@ import Button from "./button";
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <ThemeProvider>
-            <Button />
-            <Text as="span">Quill UI</Text>
-            <Heading.Hero>Quill UI</Heading.Hero>
-            <Heading.H1>Quill UI</Heading.H1>
-            <Heading.H1 as="div">Quill UI - h1 as div</Heading.H1>
-            <Heading.H1 as="span">Quill UI - h1 as span</Heading.H1>
+            <div className="quill__background--primary__type--base">
+                <Button />
+                <Text as="span">Quill UI</Text>
+                <Heading.Hero>Quill UI</Heading.Hero>
+                <Heading.H1>Quill UI</Heading.H1>
+                <Heading.H1 as="div">Quill UI - h1 as div</Heading.H1>
+                <Heading.H1 as="span">Quill UI - h1 as span</Heading.H1>
+            </div>
         </ThemeProvider>
     </React.StrictMode>,
 );
