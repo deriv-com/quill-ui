5d8e7b36fb3b1b98930922933094fbcb87321a47
ahmadtaimoor-deriv <129935294+ahmadtaimoor-deriv@users.noreply.github.com>
2024-04-02 12:58:53 +0800

    chore: fixing storybook

--- lib/components/SelectionControl/ToggleSwitch/toggle-switch.stories.tsx
+++ lib/components/SelectionControl/ToggleSwitch/toggle-switch.stories.tsx
@@ -1,37 +1,90 @@
-import React from 'react';
-import { StoryFn, Meta } from '@storybook/react';
-import ToggleSwitch, { SwitchToggleProps } from '.';
-import './toggle-switch.scss';
+import { StoryObj, Meta } from "@storybook/react";
+import ToggleSwitch from ".";
+import "./toggle-switch.scss";
+import { fn } from "@storybook/test";
 
-export default {
-  title: 'Components/ToggleSwitch',
-  component: ToggleSwitch,
-} as Meta<SwitchToggleProps>;
+const meta = {
+    title: "Components/Selection control/Toggle Switch",
+    component: ToggleSwitch,
+    parameters: { layout: "centered" },
+    tags: ["autodocs"],
+    args: {
+        defaultChecked: false,
+        disabled: false,
+        onChange: fn(),
+        onFocus: fn(),
+        onBlur: fn(),
+        wrapperClassName: "",
+    },
+    argTypes: {
+        defaultChecked: {
+            table: { type: { summary: "boolean | undefined" } },
+            description: "Flag for setting initial state",
+            control: { type: "boolean" },
+        },
+        disabled: {
+            table: { type: { summary: "boolean | undefined" } },
+            description: "Flag for setting component's disabled state",
+            control: { type: "boolean" },
+        },
+        wrapperClassName: {
+            table: { type: { summary: "string | undefined" } },
+            description: "ClassName for the switch's wrapper",
+            control: { type: "text" },
+        },
+        id: {
+            table: { type: { summary: "string | undefined" } },
+            description:
+                "Input's ID attribute value, for associating the label",
+            control: { type: "text" },
+        },
+        onChange: {
+            table: { type: { summary: "function | undefined" } },
+            description:
+                "Callback function called when the switch's value changes",
+            control: { type: null },
+        },
+        onFocus: {
+            table: { type: { summary: "function | undefined" } },
+            description: "Callback function called when the switch gains focus",
+            control: { type: null },
+        },
+        onBlur: {
+            table: { type: { summary: "function | undefined" } },
+            description: "Callback function called when the switch loses focus",
+            control: { type: null },
+        },
+    },
+} satisfies Meta<typeof ToggleSwitch>;
 
+export default meta;
 
-const Template: StoryFn<SwitchToggleProps> = (args) => <ToggleSwitch {...args} />;
+type Story = StoryObj<typeof meta>;
 
-export const Enabled = Template.bind({});
-Enabled.args = {
-  disabled: false,
-  defaultChecked: false,
+export const Enabled: Story = {
+    args: {
+        disabled: false,
+        defaultChecked: false,
+    },
 };
 
-export const Disabled = Template.bind({});
-Disabled.args = {
-  disabled: true,
-  defaultChecked: false,
+export const Disabled: Story = {
+    args: {
+        disabled: true,
+        defaultChecked: false,
+    },
 };
 
-export const Checked = Template.bind({});
-Checked.args = {
-  disabled: false,
-  defaultChecked: true,
+export const Checked: Story = {
+    args: {
+        disabled: false,
+        defaultChecked: true,
+    },
 };
 
-
-export const DisabledChecked = Template.bind({});
-DisabledChecked.args = {
-  disabled: true,
-  defaultChecked: true,
+export const DisabledChecked: Story = {
+    args: {
+        disabled: true,
+        defaultChecked: true,
+    },
 };
