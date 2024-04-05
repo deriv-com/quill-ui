1daa5699784a01c6b427d321d28b838958a8d8a9
Henry Hein <henry@regentmarkets.com>
2024-04-05 12:39:50 +0800

    fix: re-add package and package-lock

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Text, Heading, ThemeProvider } from "../lib/main";
-import Button from "./button";
+import { SnackbarWrapper } from "../lib/components/Snackbar/SnackbarWrapper";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <ThemeProvider>
-            <div className="quill__background--primary__type--base">
-                <Button />
-                <Text as="span">Quill UI</Text>
-                <Heading.Hero>Quill UI</Heading.Hero>
-                <Heading.H1>Quill UI</Heading.H1>
-                <Heading.H1 as="div">Quill UI - h1 as div</Heading.H1>
-                <Heading.H1 as="span">Quill UI - h1 as span</Heading.H1>
-            </div>
-        </ThemeProvider>
+        <div className="quill__background--primary__type--base">
+            <SnackbarWrapper />
+        </div>
     </React.StrictMode>,
 );
