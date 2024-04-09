615e029af633646bd7364ac3616cfd6cc9202b48
Akmal Djumakhodjaev <akmal@regentmarkets.com>
2024-04-09 15:40:26 +0800

    feat: radio button story updates

--- lib/components/SelectionControl/radio/radio-button/radio-button.stories.tsx
+++ lib/components/SelectionControl/radio/radio-button/radio-button.stories.tsx
@@ -2,86 +2,84 @@ import React from "react";
 import RadioButton from "./index";
 
 const RadioButtonMeta = {
-  title: "Components/Selection control/Radio/RadioButton",
-  component: RadioButton,
-  parameters: { layout: "centered" },
-  tags: ["autodocs"],
-  argTypes: {
-    id: {
-      description: "Unique identifier for the radio button element.",
+    title: "Components/Selection control/Radio/RadioButton",
+    component: RadioButton,
+    parameters: { layout: "centered" },
+    tags: ["autodocs"],
+    args: {
+        defaultChecked: true,
+        disabled: false,
+        has_info: false,
+        id: "radio-1",
+        className: "",
+        classNameLabel: "",
+        classNameInfo: "",
     },
-    defaultChecked: {
-      description: "Sets the initial checked state of the radio button.",
-      control: { type: "boolean" },
+    argTypes: {
+        id: {
+            description: "Unique identifier for the radio button element.",
+        },
+        defaultChecked: {
+            description: "Sets the initial checked state of the radio button.",
+            control: { type: "boolean" },
+        },
+        disabled: {
+            description: "Disables the radio button interaction.",
+            control: { type: "boolean" },
+        },
+        has_info: {
+            description:
+                "Displays informational content next to the radio button.",
+            control: { type: "boolean" },
+        },
+        className: {
+            description: "CSS class name applied to the radio button element.",
+        },
+        classNameLabel: {
+            description: "CSS class name applied to the radio button label.",
+        },
+        classNameInfo: {
+            description: "CSS class name applied to the informational content.",
+        },
     },
-    disabled: {
-      description: "Disables the radio button interaction.",
-      control: { type: "boolean" },
-    },
-    has_info: {
-      description: "Displays informational content next to the radio button.",
-      control: { type: "boolean" },
-    },
-    className: {
-      description: "CSS class name applied to the radio button element.",
-    },
-    classNameLabel: {
-      description: "CSS class name applied to the radio button label.",
-    },
-    classNameInfo: {
-      description: "CSS class name applied to the informational content.",
-    },
-  },
-  description: 'The RadioButton component is used to create radio button elements for user selection within a group.',
+    description:
+        "The RadioButton component is used to create radio button elements for user selection within a group.",
 };
 
 export default RadioButtonMeta;
 
 interface RadioButtonProps {
-  defaultChecked: boolean;
-  disabled: boolean;
-  has_info: boolean;
-  id: string;
-  className: string;
-  classNameLabel: string;
-  classNameInfo: string;
+    defaultChecked: boolean;
+    disabled: boolean;
+    has_info: boolean;
+    id: string;
+    className: string;
+    classNameLabel: string;
+    classNameInfo: string;
 }
 
-const Template: React.FC<RadioButtonProps> = (args) => <RadioButton {...args}>Option</RadioButton>;
+const Template: React.FC<RadioButtonProps> = (args) => (
+    <RadioButton {...args}>Option</RadioButton>
+);
 
-const defaultArgs: RadioButtonProps = {
-  defaultChecked: true,
-  disabled: false,
-  has_info: false,
-  id: "radio-1",
-  className: "",
-  classNameLabel: "",
-  classNameInfo: "",
-};
-
-export const Default = Template.bind(this, defaultArgs);
+export const Default = Template.bind({});
 
 export const Unchecked = Template.bind(this, {
-  ...defaultArgs,
-  defaultChecked: false,
+    defaultChecked: false,
 });
 
 export const CustomClassButton = Template.bind(this, {
-  ...defaultArgs,
-  className: "custom-radio",
+    className: "custom-radio",
 });
 
 export const CustomLabelClassButton = Template.bind(this, {
-  ...defaultArgs,
-  classNameLabel: "custom-label",
+    classNameLabel: "custom-label",
 });
 
 export const DisabledButton = Template.bind(this, {
-  ...defaultArgs,
-  disabled: true,
+    disabled: true,
 });
 
 export const ButtonWithInfo = Template.bind(this, {
-  ...defaultArgs,
-  has_info: true,
+    has_info: true,
 });
