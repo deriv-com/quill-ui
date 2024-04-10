d2cd5178f25c223844f777c9790a42ebd51745d1
kate-deriv <kate@regentmarkets.com>
2024-04-10 12:48:12 +0300

    refactor: add spacing to docs

--- lib/components/SelectionControl/checkbox/checkbox-group/checkbox-group.stories.tsx
+++ lib/components/SelectionControl/checkbox/checkbox-group/checkbox-group.stories.tsx
@@ -75,65 +75,65 @@ const meta = {
             table: {
                 type: {
                     summary: `
-                        Omit<ComponentProps<"input">, "placeholder" | "defaultChecked" | "size"> & {
-                        indeterminate?: boolean;
-                        showInfoIcon?: boolean;
-                        infoIconClassName?: string;
-                        id: string | number;
-                        size?: TMediumSizes;
-                        label: ReactNode;
-                        labelClassName?: string;
-                        onChange?: (
-                            e:
-                                | React.ChangeEvent<HTMLInputElement>
-                                | React.KeyboardEvent<HTMLSpanElement>,
-                        ) => void;
-                        className?: string;
-                        children?: {same as above}[]
+                        Omit<ComponentProps<"input">, "placeholder" | "defaultChecked" | "size"> & { \n
+                        indeterminate?: boolean;\n
+                        showInfoIcon?: boolean;\n
+                        infoIconClassName?: string;\n
+                        id: string | number;\n
+                        size?: TMediumSizes;\n
+                        label: ReactNode;\n
+                        labelClassName?: string;\n
+                        onChange?: (\n
+                            e:\n
+                                | React.ChangeEvent<HTMLInputElement>\n
+                                | React.KeyboardEvent<HTMLSpanElement>,\n
+                        ) => void;\n
+                        className?: string;\n
+                        children?: {same as above}[]\n
                         }[]`,
                 },
             },
             description: `Config for building Parents and Children Nodes. Each object in array represents Parent Node and includes obligational fields for id and label. Optional are fields which are named the same way as props from Checkbox (single). Also, optional is field 'children', which can include an array with fractal-like Child Nodes objects. Please note, that if you want to change config passed in props, then in order to notify CheckboxGroup component about changed you need to pass config as new one (e.g. with spread operator).
 
                 Example:
-                const config = [
-                    {
-                        label: "Parent1",
-                        id: 1,
-                        indeterminate: true,
-                        showInfoIcon: true,
-                        children: [
-                            {
-                                label: "Child1",
-                                checked: true,
-                                id: 2,
-                            },
-                            {
-                                label: "Child2",
-                                id: 3,
-                            },
-                            {
-                                label: "Child3",
-                                id: 4,
-                            },
-                        ],
-                    },
-                    {
-                        label: "Parent2",
-                        id: 5,
-                        children: [
-                            {
-                                label: "Child1",
-                                id: 6,
-                            },
-                        ],
-                    },
-                    {
-                        label: "Parent3",
-                        id: 7,
-                        showInfoIcon: true,
-                    },
-                ];`,
+            const config = [
+                {
+                    label: "Parent1",
+                    id: 1,
+                    indeterminate: true,
+                    showInfoIcon: true,
+                    children: [
+                        {
+                            label: "Child1",
+                            checked: true,
+                            id: 2,
+                        },
+                        {
+                            label: "Child2",
+                            id: 3,
+                        },
+                        {
+                            label: "Child3",
+                            id: 4,
+                        },
+                    ],
+                },
+                {
+                    label: "Parent2",
+                    id: 5,
+                    children: [
+                        {
+                            label: "Child1",
+                            id: 6,
+                        },
+                    ],
+                },
+                {
+                    label: "Parent3",
+                    id: 7,
+                    showInfoIcon: true,
+                },
+            ];`,
             control: { type: null },
         },
     },
