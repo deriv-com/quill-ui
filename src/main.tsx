1f69962678119d034edb070b2b6e2262146a8cbb
Henry Hein <henry@regentmarkets.com>
2024-04-19 15:56:40 +0800

    fix: revert

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "../lib/main";
-import { SnackbarProvider } from "../lib/providers/snackbar/snackbarProvider";
+import { Text, Heading, ThemeProvider } from "../lib/main";
+import Button from "./button";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <ThemeProvider>
             <div className="quill__background--primary__type--base">
-                <SnackbarProvider>
-                    <></>
-                </SnackbarProvider>
+            <Button />
+                <Text as="span">Quill UI</Text>
+                <Heading.Hero>Quill UI</Heading.Hero>
+                <Heading.H1>Quill UI</Heading.H1>
+                <Heading.H1 as="div">Quill UI - h1 as div</Heading.H1>
+                <Heading.H1 as="span">Quill UI - h1 as span</Heading.H1>
             </div>
         </ThemeProvider>
     </React.StrictMode>,
