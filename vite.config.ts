59af4884e4fa0e9a131f0d35de9385e8514a938a
Henry Hein <henry@regentmarkets.com>
2024-04-24 10:52:19 +0800

    fix: alias import issue

--- vite.config.ts
+++ vite.config.ts
@@ -31,14 +31,15 @@ export default defineConfig({
     },
     resolve: {
         alias: {
-            "@quill-bundle": resolve(__dirname, "lib/styles/quill-bundle"),
-            "@quill": resolve(__dirname, "lib/styles/quill"),
-            "@quill-custom": resolve(__dirname, "lib/styles/custom"),
-            "@components": resolve(__dirname, "lib/components"),
-            "@hooks": resolve(__dirname, "lib/hooks"),
-            "@providers": resolve(__dirname, "lib/providers"),
-            "@utils": resolve(__dirname, "lib/utils"),
-            "@types": resolve(__dirname, "lib/types.ts"),
+            // "@": resolve(__dirname, "./lib"),
+            "@quill-bundle": resolve(__dirname, "./lib/styles/quill-bundle"),
+            "@quill": resolve(__dirname, "./lib/styles/quill"),
+            "@quill-custom": resolve(__dirname, "./lib/styles/custom"),
+            "@components": resolve(__dirname, "./lib/components"),
+            "@hooks": resolve(__dirname, "./lib/hooks"),
+            "@providers": resolve(__dirname, "./lib/providers"),
+            "@utils": resolve(__dirname, "./lib/utils"),
+            "@types": resolve(__dirname, "./lib/types.ts"),
         },
     },
     build: {
