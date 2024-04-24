53a495329e11629351087a0ab5624bd434d13c08
Henry Hein <henry@regentmarkets.com>
2024-04-24 11:39:23 +0800

    fix: resolve comment

--- lib/components/Snackbar/index.tsx
+++ lib/components/Snackbar/index.tsx
@@ -7,22 +7,20 @@ export const SnackbarController = () => {
     const { queue } = useSnackbar();
     return (
         <div className="snackbar--container">
-            {queue.map((item: SnackbarProps, index) => {
-                if (index === 0)
-                    return (
-                        <Snackbar
-                            id={item.id}
-                            isShown={item.isShown}
-                            message={item.message}
-                            actionText={item.actionText}
-                            hasCloseButton={item.hasCloseButton}
-                            key={item.id}
-                            icon={item.icon}
-                            onActionClick={item.onActionClick}
-                        />
-                    );
-                return null;
+            {queue.slice(0, 1).map((item: SnackbarProps) => {
+                return (
+                    <Snackbar
+                        id={item.id}
+                        isShown={item.isShown}
+                        message={item.message}
+                        actionText={item.actionText}
+                        hasCloseButton={item.hasCloseButton}
+                        key={item.id}
+                        icon={item.icon}
+                        onActionClick={item.onActionClick}
+                    />
+                );
             })}
         </div>
     );
-}
+};
