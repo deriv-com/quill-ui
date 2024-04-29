a77bed13d15c91b1177a8b85279ce6b734fb6ccc
prince-deriv <prince@deriv.com>
2024-04-29 15:30:51 +0400

    fix: compatibility on lower react versions

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,9 @@ import sass from "sass";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        react(),
+        react({
+            jsxRuntime: "classic",
+        }),
         libInjectCss(),
         dts({
             include: ["lib"],
