7670699a60870bad528b6c96939c8d8f8fa8b6ed
prince-deriv <prince@deriv.com>
2024-04-29 16:00:18 +0400

    fix: removed config update

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,7 @@ import sass from "sass";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        react({
-            jsxRuntime: "classic",
-        }),
+        react(),
         libInjectCss(),
         dts({
             include: ["lib"],
