f092ca4141012e59064d7064e9efcf927e14e9c8
Akmal Djumakhodjaev <akmal@regentmarkets.com>
2024-04-30 13:43:37 +0800

    fix: split mouse and keyboard events and improve keyboard handling (#89)

--- lib/components/SelectionControl/radio/radio-button/index.tsx
+++ lib/components/SelectionControl/radio/radio-button/index.tsx
@@ -40,25 +40,30 @@ const RadioButton = ({
     ...otherProps
 }: React.PropsWithChildren<IRadio>) => {
     const [checked, setChecked] = React.useState(defaultChecked);
+    const inputRef = React.useRef<HTMLInputElement>(null);
 
     React.useEffect(() => {
         setChecked(defaultChecked);
     }, [defaultChecked]);
 
-    const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setChecked(e.target.checked);
-        onChange?.(e);
+    const handleMouseClick = () => {
+        if (!disabled) {
+            handleChange();
+        }
     };
 
-    const handleIconClick = (
-        e:
-            | React.MouseEvent<HTMLSpanElement>
-            | React.KeyboardEvent<HTMLSpanElement>,
-    ) => {
-        if (!disabled) {
-            const synthesizedEvent = new Event("change");
-            const input = e.currentTarget.previousSibling as HTMLInputElement;
-            input.dispatchEvent(synthesizedEvent);
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLSpanElement>) => {
+        if (!disabled && (e.key === "Enter" || e.key === " ")) {
+            handleChange();
+        }
+    };
+    const handleChange = () => {
+        const input = inputRef.current;
+        if (input) {
+            input.checked = !input.checked;
+            onChange?.({
+                target: input,
+            } as React.ChangeEvent<HTMLInputElement>);
         }
     };
 
@@ -94,9 +99,9 @@ const RadioButton = ({
             <input
                 className="quill-radio-button__input"
                 type="radio"
-                onChange={onInputChange}
                 checked={checked}
                 disabled={disabled}
+                ref={inputRef}
                 value={value}
                 {...otherProps}
             />
@@ -105,8 +110,8 @@ const RadioButton = ({
                 className={clsx("quill-radio-button__icon", {
                     "quill-radio-button__icon--disabled": disabled,
                 })}
-                onClick={handleIconClick}
-                onKeyDown={handleIconClick}
+                onClick={handleMouseClick}
+                onKeyDown={handleKeyDown}
                 id={id}
             >
                 {getIcon()}
@@ -121,6 +126,8 @@ const RadioButton = ({
                     },
                     classNameLabel,
                 )}
+                onClick={handleMouseClick}
+                onKeyDown={handleKeyDown}
             >
                 {children}
             </Text>
