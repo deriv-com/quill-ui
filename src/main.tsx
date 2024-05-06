3bd92b02fedf15bfe0e3f5955e6e6c7e863aaabf
Nuzhy-Deriv <nuzhy@deriv.com>
2024-05-06 13:50:10 +0800

    fix: set message font-family to IBMPlexs

--- src/main.tsx
+++ src/main.tsx
@@ -12,18 +12,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
                 <Heading.H1>Quill UI</Heading.H1>
                 <Heading.H1 as="div">Quill UI - h1 as div</Heading.H1>
                 <Heading.H1 as="span">Quill UI - h1 as span</Heading.H1>
-                <div style={{ width: "20rem" }}>
-                    <TextField
-                        type="tel"
-                        variant="fill"
-                        value="hello"
-                        status="success"
-                        label="hello"
-                        maxLength={10}
-                        message="message here message here message here message here message here message here"
-                        characterCounter
-                    />
-                </div>
             </div>
         </ThemeProvider>
     </React.StrictMode>,
