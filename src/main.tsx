5f00434374fc43636dcbf1852eff37bcf441e996
prince-deriv <82309725+prince-deriv@users.noreply.github.com>
2024-05-06 10:00:21 +0400

    chore: toggling and updating improvement (#113)

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Heading, ThemeProvider } from "../lib/main";
+import { Heading } from "../lib/main";
 import ThemeSwitcher from "./theme-switcher";
+import ThemeRenderer from "./theme-renderer";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <ThemeProvider>
+        <ThemeRenderer>
             <div className="quill__background--primary__type--base">
                 <ThemeSwitcher />
                 <Heading.Hero>Quill UI</Heading.Hero>
@@ -13,6 +14,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
                 <Heading.H1 as="div">Quill UI - h1 as div</Heading.H1>
                 <Heading.H1 as="span">Quill UI - h1 as span</Heading.H1>
             </div>
-        </ThemeProvider>
+        </ThemeRenderer>
     </React.StrictMode>,
 );
