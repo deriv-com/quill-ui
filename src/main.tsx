92989c37795226b249e6906c1f003275521ef37a
jia-deriv <jiawei@regentmarkets.com>
2024-05-08 11:41:22 +0800

    chore: remove testing code

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { Heading } from "../lib/main";
 import ThemeSwitcher from "./theme-switcher";
 import ThemeRenderer from "./theme-renderer";
-import DropdownChipMultiSelect from "@components/Chip/dropdown-chip-multi-select";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
@@ -14,22 +13,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
                 <Heading.H1>Quill UI</Heading.H1>
                 <Heading.H1 as="div">Quill UI - h1 as div</Heading.H1>
                 <Heading.H1 as="span">Quill UI - h1 as span</Heading.H1>
-                <DropdownChipMultiSelect
-                    label="Testing"
-                    onSelectionChange={(value) => {
-                        console.log(value);
-                    }}
-                    options={[
-                        { value: "1", label: "Sample Item 1" },
-                        { value: "2", label: "Sample Item 2" },
-                        {
-                            value: "3",
-                            label: "Sample Item 3 - which is disabled",
-                            disabled: true,
-                        },
-                        { value: "4", label: "Sample Item 4" },
-                    ]}
-                />
             </div>
         </ThemeRenderer>
     </React.StrictMode>,
