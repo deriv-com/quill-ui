9cac70300010270b38e4ea7c20f86b7344fc5862
prince-deriv <prince@deriv.com>
2024-05-13 15:54:14 +0400

    chore: added persistent theme class loading

--- lib/providers/theme/themeProvider.tsx
+++ lib/providers/theme/themeProvider.tsx
@@ -5,13 +5,24 @@ import { Theme, ThemeContext } from "./themeContext";
 export interface ThemeProviderProps {
     children: React.ReactNode;
     theme?: Theme;
+    persistent?: boolean;
 }
 
-export const ThemeProvider = ({ children, theme }: ThemeProviderProps) => {
+export const ThemeProvider = ({
+    children,
+    theme,
+    persistent,
+}: ThemeProviderProps) => {
     const [selectedTheme, setSelectedTheme] = useState<Theme | undefined>(
         theme,
     );
+
     const [currentTheme, setCurrentTheme] = useState<Theme>(theme ?? "light");
+
+    const themeClassNames = ["light", "dark"];
+
+    const root =
+        typeof document !== "undefined" ? document.documentElement : null;
 
     const systemPrefersDark = useMediaQuery("(prefers-color-scheme: dark)");
 
@@ -22,20 +33,59 @@ export const ThemeProvider = ({ children, theme }: ThemeProviderProps) => {
         setSelectedTheme(selectedNewTheme);
     };
 
-    useEffect(() => {
+    const ApplyTheme = () => {
         const currentTheme =
             selectedTheme || (systemPrefersDark ? "dark" : "light");
         const unusedTheme = currentTheme === "dark" ? "light" : "dark";
-        const root = document.documentElement;
-        root.classList.add(currentTheme);
-        root.classList.remove(unusedTheme);
+
+        root?.classList.add(currentTheme);
+        root?.classList.remove(unusedTheme);
 
         setCurrentTheme(currentTheme);
+        setSelectedTheme(currentTheme);
+    };
+
+    useEffect(() => {
+        ApplyTheme();
     }, [selectedTheme, systemPrefersDark]);
 
     useEffect(() => {
         setSelectedTheme(theme);
     }, [theme]);
+
+    useEffect(() => {
+        /* 
+            If enabled, this feature allows for the continuous addition of a class name to the root element 
+            to manage the theme. This can be particularly useful for consumers with extensive mutations 
+            to the root document before it is fully loaded. 
+        */
+        if (persistent) {
+            const observer = new MutationObserver((mutationsList) => {
+                mutationsList.forEach((mutation) => {
+                    if (
+                        mutation.type === "attributes" &&
+                        mutation.attributeName === "class"
+                    ) {
+                        if (
+                            !themeClassNames.some((className) =>
+                                root?.classList.contains(className),
+                            )
+                        ) {
+                            ApplyTheme();
+                        }
+                    }
+                });
+            });
+
+            if (root) {
+                observer.observe(root, { attributes: true });
+            }
+
+            return () => {
+                observer.disconnect();
+            };
+        }
+    }, []);
 
     return (
         <ThemeContext.Provider value={{ theme: currentTheme, toggleTheme }}>
