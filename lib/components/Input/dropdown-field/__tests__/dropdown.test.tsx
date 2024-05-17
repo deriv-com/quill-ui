93bc494eb6486823e1da42a434ed0b23d73a1ef2
meenakshi-deriv <meenakshi@regentmarkets.com>
2024-05-17 12:33:46 +0400

    fix: updated test cases

---
+++ lib/components/Input/dropdown-field/__tests__/dropdown.test.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import InputDropdown from "../index";
+
+// Mock options for dropdown
+const options = [
+    { value: "1", text: "Option 1" },
+    { value: "2", text: "Option 2" },
+    { value: "3", text: "Option 3" },
+];
+
+describe("InputDropdown component", () => {
+    it("renders dropdown with options", () => {
+        const dropdownProps = {
+            options: options,
+            onSelectOption: (value: string) => {
+                return value;
+            },
+        };
+        render(<InputDropdown {...dropdownProps} />);
+        fireEvent.click(screen.getByTestId("dropdown-input"));
+
+        options.forEach((option) => {
+            expect(screen.getByText(option.text)).toBeInTheDocument();
+        });
+    });
+
+    it("selects an option from dropdown", () => {
+        const dropdownProps = {
+            options: options,
+            onSelectOption: (value: string) => {
+                return value;
+            },
+        };
+        render(<InputDropdown {...dropdownProps} />);
+
+        fireEvent.click(screen.getByTestId("dropdown-input"));
+
+        fireEvent.click(screen.getByText(options[0].text));
+
+        expect(screen.getByTestId("dropdown-input")).toHaveValue(
+            options[0].text,
+        );
+    });
+
+    it("closes dropdown on selection", () => {
+        const dropdownProps = {
+            options: options,
+            onSelectOption: (value: string) => {
+                return value;
+            },
+        };
+        render(<InputDropdown {...dropdownProps} />);
+
+        fireEvent.click(screen.getByTestId("dropdown-input"));
+
+        fireEvent.click(screen.getByText(options[0].text));
+
+        expect(screen.queryByText(options[0].text)).not.toBeInTheDocument();
+    });
+    it("should render a TextField with success status", () => {
+        const dropdownProps = {
+            options: options,
+            onSelectOption: (value: string) => {
+                return value;
+            },
+        };
+        const { container } = render(
+            <InputDropdown {...dropdownProps} status="success" />,
+        );
+        expect(container).toMatchSnapshot();
+    });
+
+    it("should render a TextField with error status", () => {
+        const dropdownProps = {
+            options: options,
+            onSelectOption: (value: string) => {
+                return value;
+            },
+        };
+        const { container } = render(
+            <InputDropdown {...dropdownProps} status="error" />,
+        );
+        expect(container).toMatchSnapshot();
+    });
+});
