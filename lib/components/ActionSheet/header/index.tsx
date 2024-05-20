612e44aa4743c4d13361f98638221327061d3542
kate-deriv <121025168+kate-deriv@users.noreply.github.com>
2024-05-20 08:28:05 +0300

    DTRA / Kate / Action sheet refactor: expand types and change css (#146)

--- lib/components/ActionSheet/header/index.tsx
+++ lib/components/ActionSheet/header/index.tsx
@@ -4,9 +4,9 @@ import clsx from "clsx";
 import "./header.scss";
 import { Heading, Text } from "@components/Typography";
 
-interface HeaderProps extends ComponentPropsWithoutRef<"div"> {
-    title?: string;
-    description?: string;
+interface HeaderProps extends Omit<ComponentPropsWithoutRef<"div">, "title"> {
+    title?: ReactNode;
+    description?: ReactNode;
     icon?: ReactNode;
     closeIcon?: ReactNode;
 }
@@ -33,18 +33,26 @@ const Header = ({
             {...rest}
         >
             <div className="quill-action-sheet--title">
-                <div className="quill-action-sheet--title--icon">{Icon}</div>
-                <Heading.H5>{title}</Heading.H5>
-                <button
-                    className="quill-action-sheet--title--icon quill-action-sheet--title--icon--close"
-                    onClick={handleClose}
-                >
-                    {CloseIcon}
-                </button>
+                {Icon && (
+                    <div className="quill-action-sheet--title--icon">
+                        {Icon}
+                    </div>
+                )}
+                {title && <Heading.H5>{title}</Heading.H5>}
+                {CloseIcon && (
+                    <button
+                        className="quill-action-sheet--title--icon quill-action-sheet--title--icon--close"
+                        onClick={handleClose}
+                    >
+                        {CloseIcon}
+                    </button>
+                )}
             </div>
-            <Text className="quill-action-sheet--description">
-                {description}
-            </Text>
+            {description && (
+                <Text className="quill-action-sheet--description">
+                    {description}
+                </Text>
+            )}
         </div>
     );
 };
