71fae8d60b34b2e11cee85c035ba70b869943a1a
Akmal Djumakhodjaev <akmal@regentmarkets.com>
2024-06-06 13:45:24 +0800

    fix: add border style for smol size

--- lib/components/Input/text-field-with-steppers/index.tsx
+++ lib/components/Input/text-field-with-steppers/index.tsx
@@ -16,13 +16,13 @@ export interface TextFieldWithSteppersProps extends ComponentProps<typeof Input>
 
 export const TextFieldWithSteppers = forwardRef<HTMLInputElement, TextFieldWithSteppersProps>(
     (props, ref) => {
-        const { value, onChange, disabled, textAlignment, placeholder = 0, unitLeft = '', unitRight = '', decimals = 2, ...rest } = props;
+        const { value, onChange, disabled, textAlignment, placeholder = 0, unitLeft = '', unitRight = '', decimals = 2, inputSize, ...rest } = props;
 
         const getFormatValue = (value: number) => parseFloat(value.toFixed(decimals));
 
         const steppersSectionRight = (
             <>
-                {textAlignment !== 'center' && (<button className={clsx("quill-input-steppers-button", "quill-input-steppers-button--decrement")}
+                {textAlignment !== 'center' && (<button className={clsx("quill-input-steppers-button", "quill-input-steppers-button--decrement", `quill-input-steppers-button--${inputSize}`)}
                     onClick={() => {
                             onChange?.({
                                 target: { value: getFormatValue(Number(value) - 1) },
@@ -32,7 +32,7 @@ export const TextFieldWithSteppers = forwardRef<HTMLInputElement, TextFieldWithS
                     >
                     <LabelPairedMinusSmRegularIcon />
                 </button>)}
-                <button className={clsx("quill-input-steppers-button", "quill-input-steppers-button--increment")}
+                <button className={clsx("quill-input-steppers-button", "quill-input-steppers-button--increment", `quill-input-steppers-button--${inputSize}`)}
                     onClick={() => {
                             onChange?.({
                                 target: { value: getFormatValue(Number(value) + 1) },
@@ -47,7 +47,7 @@ export const TextFieldWithSteppers = forwardRef<HTMLInputElement, TextFieldWithS
 
         const steppersSectionLeft = (
             <>
-                <button className={clsx("quill-input-steppers-button", "quill-input-steppers-button--decrement")}
+                <button className={clsx("quill-input-steppers-button", "quill-input-steppers-button--decrement", `quill-input-steppers-button--${inputSize}`)}
                     onClick={() => {
                             onChange?.({
                                 target: { value: getFormatValue(Number(value) - 1) },
@@ -66,6 +66,7 @@ export const TextFieldWithSteppers = forwardRef<HTMLInputElement, TextFieldWithS
                     {...rest}
                     ref={ref}
                     disabled={disabled}
+                    inputSize={inputSize}
                     leftIcon={textAlignment === 'center' && steppersSectionLeft}
                     leftPlaceholder={unitLeft}
                     onChange={onChange}
