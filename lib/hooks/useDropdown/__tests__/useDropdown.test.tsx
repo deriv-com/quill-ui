bb5df6432ab5c305eb9a80d96e24e8fd02963686
jia-deriv <jiawei@regentmarkets.com>
2024-06-24 16:47:21 +0800

    chore: add test case for use dropdown hook

---
+++ lib/hooks/useDropdown/__tests__/useDropdown.test.tsx
@@ -0,0 +1,45 @@
+import { renderHook, act, render } from "@testing-library/react";
+import useDropdown from ".."; // Adjust the path to your actual file
+import userEvent from "@testing-library/user-event";
+
+describe("useDropdown", () => {
+    test("should initialize as closed", () => {
+        const { result } = renderHook(() => useDropdown());
+        const { isOpen } = result.current;
+        expect(isOpen).toBe(false);
+    });
+
+    test("should close the dropdown when open and close is called", () => {
+        const { result } = renderHook(() => useDropdown());
+
+        act(() => {
+            result.current.open();
+        });
+
+        expect(result.current.isOpen).toBe(true);
+
+        act(() => {
+            result.current.close();
+        });
+        expect(result.current.isOpen).toBe(false);
+    });
+
+    test("should close the dropdown when clicking outside", async () => {
+        const { result } = renderHook(() => useDropdown());
+
+        const { getByText } = render(
+            <div>
+                <div ref={result.current.ref} onClick={result.current.open}>
+                    inside
+                </div>
+                <button>outside</button>
+            </div>,
+        );
+
+        await userEvent.click(getByText("inside"));
+        expect(result.current.isOpen).toBe(true);
+
+        await userEvent.click(getByText("outside"));
+        expect(result.current.isOpen).toBe(false);
+    });
+});
--- lib/hooks/useDropdown/index.ts
+++ lib/hooks/useDropdown/index.ts
@@ -1,7 +1,5 @@
 import { useState, useCallback, useRef } from "react";
-import { useOnClickOutside, useEventListener } from "usehooks-ts";
-
-const ESC_KEY = "27";
+import { useOnClickOutside } from "usehooks-ts";
 
 const useDropdown = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -11,12 +9,6 @@ const useDropdown = () => {
     const close = useCallback(() => setIsOpen(false), []);
 
     useOnClickOutside(ref, close);
-
-    useEventListener("keydown", (event) => {
-        if (event.code === ESC_KEY) {
-            close();
-        }
-    });
 
     return { ref, isOpen, open, close };
 };
