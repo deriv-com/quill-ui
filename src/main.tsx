2acd23eb642bcac92a990966f00d36f2cea7b672
jia-deriv <jiawei@regentmarkets.com>
2024-06-24 21:03:31 +0800

    chore: cleanup code

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { Heading } from "../lib/main";
 import ThemeSwitcher from "./theme-switcher";
 import ThemeRenderer from "./theme-renderer";
-import TestingBreakpoints from "./check-breakpoints";
 import BreakpointProvider from "@providers/breakpoint/breakpointProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -12,7 +11,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
             <BreakpointProvider>
                 <div className="quill__background--primary__type--base">
                     <ThemeSwitcher />
-                    <TestingBreakpoints />
                     <Heading.Hero>Quill UI</Heading.Hero>
                     <Heading.H1>Quill UI</Heading.H1>
                     <Heading.H1 as="div">Quill UI - h1 as div</Heading.H1>
