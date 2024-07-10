b63eea3b6d31ff9fbfd2b04e1ecdb81a610d4644
jia-deriv <jiawei@regentmarkets.com>
2024-07-10 17:03:55 +0800

    chore: add portal for snackbar controller

--- lib/components/Snackbar/snackbar-controller.tsx
+++ lib/components/Snackbar/snackbar-controller.tsx
@@ -1,27 +1,28 @@
 import React from "react";
 import { Snackbar, SnackbarProps } from "@components/Snackbar/snackbar";
 import { useSnackbar } from "@hooks/useSnackbar";
+import { createPortal } from "react-dom";
 import "./snackbar-controller.scss";
 
 export const SnackbarController = () => {
     const { queue } = useSnackbar();
-    return (
+
+    return createPortal(
         <div className="snackbar--container">
-            {queue.slice(0, 1).map((item: SnackbarProps) => {
-                return (
-                    <Snackbar
-                        id={item.id}
-                        isVisible={item.isVisible}
-                        message={item.message}
-                        actionText={item.actionText}
-                        hasCloseButton={item.hasCloseButton}
-                        key={item.id}
-                        icon={item.icon}
-                        onActionClick={item.onActionClick}
-                        standalone={false}
-                    />
-                );
-            })}
-        </div>
+            {queue.slice(0, 1).map((item: SnackbarProps) => (
+                <Snackbar
+                    id={item.id}
+                    isVisible={item.isVisible}
+                    message={item.message}
+                    actionText={item.actionText}
+                    hasCloseButton={item.hasCloseButton}
+                    key={item.id}
+                    icon={item.icon}
+                    onActionClick={item.onActionClick}
+                    standalone={false}
+                />
+            ))}
+        </div>,
+        document.body,
     );
 };
