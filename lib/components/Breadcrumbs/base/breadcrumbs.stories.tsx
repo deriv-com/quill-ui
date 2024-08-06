94f20b6a3e11378a0f3e8bcdfbe5a2ab97bfae66
meenakshi-deriv <meenakshi@regentmarkets.com>
2024-08-06 18:26:55 +0400

    fix: breadcrumb component

---
+++ lib/components/Breadcrumbs/base/breadcrumbs.stories.tsx
@@ -0,0 +1,56 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Base } from ".";
+
+const meta = {
+    title: "Components/Breadcrumbs/Default",
+    tags: ["auto-docs"],
+    component: Base,
+    parameters: {
+        layout: "centered",
+    },
+} satisfies Meta<typeof Base>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        links: [
+            {
+                href: "",
+                content: "Home",
+            },
+            {
+                href: "",
+                content: "Trade",
+            },
+        ],
+    },
+};
+
+export const Max: Story = {
+    args: {
+        links: [
+            {
+                href: "",
+                content: "Home",
+            },
+            {
+                href: "/help-centre",
+                content: "Help Centre",
+            },
+            {
+                href: "/help-centre/account",
+                content: "Account",
+            },
+            {
+                href: "/help-centre/account/questions",
+                content: "Questions",
+            },
+            {
+                href: "",
+                content: "How to trade in Deriv",
+            },
+        ],
+    },
+};
---
+++ lib/components/Breadcrumbs/base/index.tsx
@@ -0,0 +1,84 @@
+import { BreadcrumbProps, LinkProps } from "../types";
+import { Fragment, useEffect, useState } from "react";
+import { maxLinks } from "./constants";
+import "./breadcrumbs.scss";
+import useBreakpoints from "@hooks/useBreakpoints";
+import clsx from "clsx";
+import Link from "@components/Link";
+
+export function Base({ size = "sm", links = [], className }: BreadcrumbProps) {
+    const [renderLinks, setRenderLinks] = useState<LinkProps[]>([]);
+    const [dropdownLinks, setDropdownLinks] = useState<LinkProps[]>([]);
+
+    const isLastItem = (key: number) => renderLinks.length - 1 > key;
+    const linksLen = links.length;
+    const { isMobile } = useBreakpoints();
+    const hasExtra = isMobile && maxLinks < linksLen;
+
+    useEffect(() => {
+        setRenderLinks(links);
+
+        if (hasExtra) {
+            const limitedLinks = [
+                links[0],
+                { content: "...", href: "" },
+                links[linksLen - 2],
+                links[linksLen - 1],
+            ];
+
+            setRenderLinks(limitedLinks);
+
+            const remainingLinks: LinkProps[] = links.filter(
+                (item) => !new Set(limitedLinks).has(item),
+            );
+
+            setDropdownLinks(remainingLinks);
+        }
+    }, [links]);
+
+    return (
+        <div className={clsx("breadcrumb--container", className)}>
+            {renderLinks.map(({ content, href }, lk) => (
+                <Fragment key={`breadcrumbs-${content}`}>
+                    {hasExtra && lk === 1 && (
+                        <select
+                            className="breadcrumb--dropdown"
+                            onChange={(
+                                e: React.ChangeEvent<HTMLSelectElement>,
+                            ) => {
+                                if (typeof window !== "undefined") {
+                                    window.location.href = e.target.value;
+                                }
+                            }}
+                        >
+                            {dropdownLinks.map(({ href, content }) => {
+                                return (
+                                    <option
+                                        key={`dropdown-${content}`}
+                                        value={href}
+                                    >
+                                        {content}
+                                    </option>
+                                );
+                            })}
+                        </select>
+                    )}
+
+                    <Link
+                        size={size}
+                        href={href}
+                        hasHoverEffect={false}
+                        hasChevron={isLastItem(lk)}
+                        color="black"
+                        disabled={!isLastItem(lk)}
+                        className="breadcrumb--item"
+                    >
+                        {content}
+                    </Link>
+                </Fragment>
+            ))}
+        </div>
+    );
+}
+
+export default Base;
