128db8d7d05c9763b2892b0d7508ac81ed91c9f7
jia-deriv <jiawei@regentmarkets.com>
2024-08-08 14:31:20 +0800

    fix: dropdown button behaviour

---
+++ lib/components/Atom/dropdown/item-container/index.tsx
@@ -0,0 +1,37 @@
+import React, { ComponentProps, forwardRef } from "react";
+import "./item-container.scss";
+import clsx from "clsx";
+import { TMediumSizes, TRegularSizes } from "@types";
+import { createPortal } from "react-dom";
+
+export interface ItemContainerProps extends Omit<ComponentProps<"div">, "ref"> {
+    size?: TMediumSizes;
+    height?: TRegularSizes;
+    portalContainer?: Element | DocumentFragment;
+}
+
+export const ItemContainer = forwardRef<HTMLDivElement, ItemContainerProps>(
+    (
+        { size = "md", height = "md", className, portalContainer, ...rest },
+        ref,
+    ) => {
+        const content = (
+            <div
+                className={clsx(
+                    "quill__item-container",
+                    `quill__item-container--size-${size}`,
+                    `quill__item-container--height-${height}`,
+                    className,
+                )}
+                ref={ref}
+                {...rest}
+            />
+        );
+
+        if (portalContainer) {
+            return createPortal(content, portalContainer);
+        }
+
+        return content;
+    },
+);
--- lib/components/Button/dropdown/dropdown-button.stories.tsx
+++ lib/components/Button/dropdown/dropdown-button.stories.tsx
@@ -4,9 +4,15 @@ import { DropdownButton } from ".";
 const meta = {
     title: "Components/Button/Dropdown",
     component: DropdownButton,
-
+    parameters: {
+        docs: {
+            story: {
+                height: "300px",
+            },
+        },
+    },
     args: {
-        defaultOption: { value: "", label: "Dropdown menu" },
+        label: "Label",
         disabled: false,
     },
     argTypes: {
@@ -28,9 +34,16 @@ const meta = {
             options: ["sm", "md", "lg", "xl"],
             control: { type: "radio" },
         },
-
         color: {
-            options: ["coral", "black", "white", "purchase", "sell"],
+            options: [
+                "coral",
+                "black",
+                "white",
+                "purchase",
+                "sell",
+                "black-white",
+                "white-black",
+            ],
             control: { type: "radio" },
         },
         onClick: {
@@ -60,6 +73,21 @@ export const DropdownMenu: Story = {
                 label: "Sample Item 2",
             },
             { value: "3", label: "Sample Item 3" },
+            { value: "4", label: "Sample Item 4" },
+            { value: "4", label: "Sample Item 4" },
+            { value: "4", label: "Sample Item 4" },
+            { value: "4", label: "Sample Item 4" },
+            { value: "4", label: "Sample Item 4" },
+            { value: "4", label: "Sample Item 4" },
+            { value: "4", label: "Sample Item 4" },
+            { value: "4", label: "Sample Item 4" },
+            { value: "4", label: "Sample Item 4" },
+            { value: "4", label: "Sample Item 4" },
+            { value: "4", label: "Sample Item 4" },
+            { value: "4", label: "Sample Item 4" },
+            { value: "4", label: "Sample Item 4" },
+            { value: "4", label: "Sample Item 4" },
+            { value: "4", label: "Sample Item 4" },
             { value: "4", label: "Sample Item 4" },
         ],
     },
