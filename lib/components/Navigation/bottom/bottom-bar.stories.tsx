4e0a0edc5a39aa27b4deb9a1c4e601fdcd43a295
jia-deriv <jiawei@regentmarkets.com>
2024-08-19 16:50:01 +0800

    feat: add bottom navigation and storybook

---
+++ lib/components/Navigation/bottom/bottom-bar.stories.tsx
@@ -0,0 +1,118 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import {
+    StandaloneGearFillIcon,
+    StandaloneGearRegularIcon,
+    StandaloneHouseBlankFillIcon,
+    StandaloneHouseBlankRegularIcon,
+    StandalonePlaceholderFillIcon,
+    StandalonePlaceholderRegularIcon,
+    StandaloneUserFillIcon,
+    StandaloneUserRegularIcon,
+} from "@deriv/quill-icons";
+import Navigation from "..";
+import BottomBar from "./bottom-bar";
+import BottomAction from "./bottom-action";
+import React from "react";
+import { Text } from "@components/Typography";
+
+const length = 4;
+
+type Template = React.ComponentProps<typeof BottomBar & typeof BottomAction>;
+
+const icons: Record<string, object> = {
+    placeholder: {
+        active: <StandalonePlaceholderFillIcon iconSize="sm" />,
+        default: <StandalonePlaceholderRegularIcon iconSize="sm" />,
+    },
+    home: {
+        active: <StandaloneHouseBlankFillIcon iconSize="sm" />,
+        default: <StandaloneHouseBlankRegularIcon iconSize="sm" />,
+    },
+    profile: {
+        active: <StandaloneUserFillIcon iconSize="sm" />,
+        default: <StandaloneUserRegularIcon iconSize="sm" />,
+    },
+    settings: {
+        active: <StandaloneGearFillIcon iconSize="sm" />,
+        default: <StandaloneGearRegularIcon iconSize="sm" />,
+    },
+};
+
+const meta = {
+    title: "Components/Navigation/BottomBar",
+    tags: ["autodocs"],
+    args: {
+        label: "Label",
+        icon: <StandalonePlaceholderRegularIcon iconSize="sm" />,
+        activeIcon: <StandalonePlaceholderFillIcon iconSize="sm" />,
+        showLabels: false,
+        value: 0,
+        customIcon: "home",
+    },
+    argTypes: {
+        customIcon: {
+            description: "icon in each bottom bar action",
+            options: Object.keys(icons),
+            mapping: icons,
+            control: {
+                type: "radio",
+            },
+        },
+        showLabels: {
+            control: "boolean",
+        },
+    },
+} satisfies Meta<typeof BottomBar & typeof BottomAction>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const Template: React.FC<Template> = ({ ...args }: Template) => {
+    const { value, showLabels, customIcon, ...rest } = args;
+    const [index, setIndex] = React.useState(value);
+
+    const Content = () => (
+        <div
+            style={{
+                backgroundColor:
+                    "var(--semantic-color-slate-solid-surface-frame-low)",
+                height: "500px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
+            <Text>This is content in Menu {index + 1}</Text>
+        </div>
+    );
+
+    React.useEffect(() => {
+        setIndex(value);
+    }, [value]);
+
+    return (
+        <>
+            <Content />
+            <Navigation.Bottom
+                value={index}
+                showLabels={showLabels}
+                onChange={(_, newValue) => {
+                    setIndex(newValue);
+                }}
+            >
+                {Array.from({ length }, () => {
+                    return (
+                        <Navigation.BottomAction
+                            {...rest}
+                            activeIcon={customIcon.active}
+                            icon={customIcon.default}
+                        />
+                    );
+                })}
+            </Navigation.Bottom>
+        </>
+    );
+};
+
+export const BottomNavigationBarDefault = Template.bind(this) as Story;
+BottomNavigationBarDefault.args = { ...meta.args };
---
+++ lib/components/Navigation/bottom/bottom-bar.tsx
@@ -0,0 +1,58 @@
+import clsx from "clsx";
+import React from "react";
+import { BottomActionProps } from "./bottom-action";
+import "./bottom-navigation.scss";
+import "@deriv-com/quill-tokens/dist/quill.css";
+
+export interface BottomBarProps
+    extends React.ComponentProps<React.ElementType> {
+    showLabels?: boolean;
+    as?: React.ElementType;
+    value?: number;
+    onChange?: (event: React.ChangeEvent<HTMLElement>, value: number) => void;
+}
+
+const BottomBar = ({
+    as: Element = "div",
+    className,
+    children,
+    onChange,
+    value,
+    showLabels,
+    ...rest
+}: BottomBarProps) => {
+    return (
+        <Element
+            className={clsx(
+                "quill-navigation-bottom-bar__container",
+                className,
+            )}
+            {...rest}
+        >
+            {React.Children.map(
+                children,
+                (child: React.ReactNode, childIndex) => {
+                    if (!React.isValidElement<BottomActionProps>(child))
+                        return null;
+
+                    const childValue =
+                        child.props.value === undefined
+                            ? childIndex
+                            : child.props.value;
+
+                    return React.cloneElement(child, {
+                        selected: childValue === value,
+                        showLabel:
+                            child.props.showLabel !== undefined
+                                ? child.props.showLabel
+                                : showLabels,
+                        value: childValue,
+                        onChange,
+                    });
+                },
+            )}
+        </Element>
+    );
+};
+
+export default BottomBar;
