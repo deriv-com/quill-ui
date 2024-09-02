b80358c69c65ff575355f23ad9d26342ca299090
jia-deriv <jiawei@regentmarkets.com>
2024-09-02 17:14:29 +0800

    chore: update readme

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,7 +11,6 @@ const config: StorybookConfig = {
         "@storybook/addon-interactions",
         "@storybook/addon-themes",
         "@storybook/addon-mdx-gfm",
-        "@chromatic-com/storybook"
     ],
 
     framework: {
