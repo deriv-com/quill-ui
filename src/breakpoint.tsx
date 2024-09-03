1c943c772498565e314eb626ffd7c1e572370360
jia-deriv <jiawei@regentmarkets.com>
2024-09-03 15:58:36 +0800

    chore: cleanup code

--- src/breakpoint.tsx
+++ src/breakpoint.tsx
@@ -1,14 +1,11 @@
 import useBreakpoints from "@hooks/useBreakpoints";
-import useTheme from "@hooks/useTheme";
 import React from "react";
 
 const Breakpoint = () => {
     const { isDesktop, isMobile, isTablet } = useBreakpoints();
-    const { toggleTheme } = useTheme();
 
     return (
         <div>
-            <button onClick={toggleTheme}>Toggle</button>
             {isDesktop && "Desktop"}
             {isMobile && "Mobile"}
             {isTablet && "Tablet"}
