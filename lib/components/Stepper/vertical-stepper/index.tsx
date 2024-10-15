f77e26a759ecc53e1a9f51679ecccfa2772f2d51
maryia-matskevich-deriv <103181650+maryia-matskevich-deriv@users.noreply.github.com>
2024-10-15 13:59:42 +0300

    fix: import Stepper css (#471)

--- lib/components/Stepper/vertical-stepper/index.tsx
+++ lib/components/Stepper/vertical-stepper/index.tsx
@@ -1,5 +1,6 @@
 import { BasicStepper } from '../index';
 import { TBasicStepperProps } from '../types';
+import "../stepper.scss";
 
 export const VerticalStepper = (props: TBasicStepperProps) => {
     return <BasicStepper {...props} orientation="vertical" />;
